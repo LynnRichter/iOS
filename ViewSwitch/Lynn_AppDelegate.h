7dd63f2300c32917e4bae8393c35cc8598b322b5
LynnRichter <swfc_mwl@hotmail.com>
2013-07-24 10:11:45 +0800

    Initial Commit

---
+++ ViewSwitch/Lynn_AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  Lynn_AppDelegate.h
+//  ViewSwitch
+//
+//  Created by Lynnrichter on 13-7-24.
+//  Copyright (c) 2013年 Lynn Richter. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class Lynn_ViewController;
+
+@interface Lynn_AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) Lynn_ViewController *viewController;
+
+@end
---
+++ ViewSwitch/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ViewSwitch
+//
+//  Created by Lynnrichter on 13-7-24.
+//  Copyright (c) 2013年 Lynn Richter. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "Lynn_AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([Lynn_AppDelegate class]));
+    }
+}
